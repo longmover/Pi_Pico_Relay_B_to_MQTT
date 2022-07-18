d5b85f1e83cd835a3fdd228ffbc1aced15cabf3e
longmover <sjd113@gmail.com>
2022-07-18 19:34:18 +0100

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,10 @@
+secrets = {
+    'WLAN_SSID': '[wifi_ssid]',
+    'WLAN_PWD': '[wifi_passphrase]',
+    'TIMEZONE' : 'Europe/London',
+    'MQTT_USER' : '[mqtt_username]',
+    'MQTT_PWD' : '[mqtt_password]',
+    'MQTT_BROKER' : '[mqtt_broker_ip]',
+    'MQTT_PORT' : 1883,
+    'MQTT_DEVICE_NAME' : 'picoRelay'
+    }
